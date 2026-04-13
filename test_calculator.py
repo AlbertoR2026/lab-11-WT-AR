faae15ca18a45c451100ea795cfb8b4463c0deab
AlbertoR2026 <alberto.rios@ufl.edu>
2026-04-13 12:46:57 -0400

    alberto unit tests complete, pending  William unit test & functions in calculator.py to cause merge error, after we fix the merge error we can submit

--- test_calculator.py
+++ test_calculator.py
@@ -1,54 +1,59 @@
+# https://github.com/AlbertoR2026/lab-10-WT-AR
+# Partner 1: William Tong
+# Partner 2: Alberto Rios
+
+print("Running test cases")
 import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    ######### Partner 2
+
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(sub(5, 3), 2)
+
+    ##########################
 
     ######## Partner 1
+
     # def test_multiply(self): # 3 assertions
     #     fill in code
 
     # def test_divide(self): # 3 assertions
     #     fill in code
-    # ##########################
 
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    ##########################
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    ######## Partner 2 (alberto)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
-    
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
+
+    def test_logarithm(self):
+        self.assertAlmostEqual(log(10, 100), 2)
+
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
+            log(1, 10)
+
+    ##########################
+
     ######## Partner 1
+
     # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
     #     fill in code
 
     # def test_hypotenuse(self): # 3 assertions
     #     fill in code
 
     # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
     #     fill in code
+
     ##########################
 
 # Do not touch this
