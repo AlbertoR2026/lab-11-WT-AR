af10cecfe70bd9821faac1ad8aa8cee60da61f0a
AlbertoR2026 <alberto.rios@ufl.edu>
2026-04-13 11:56:16 -0400

    Alberto Rios function pending testcases

--- calculator.py
+++ calculator.py
@@ -5,7 +5,35 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+
+import math
+
+# https://github.com/AlbertoR2026/lab-10-WT-AR
+# Partner 1: William Tong
+# Partner 2: Alberto Rios
+
+import math
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Invalid input for logarithm")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
 
 
