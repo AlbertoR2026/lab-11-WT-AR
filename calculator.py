748a2e9362ab9a97bbea454afa5a395cebbc9b3f
William Tong <william.xin.tong@gmail.com>
2026-04-13 14:44:59 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -14,6 +14,14 @@ import math
 
 import math
 
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of a negative number")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    math.hypot(a, b)
+
 def add(a, b):
     return a + b
 
